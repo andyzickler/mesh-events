857c8585b2f223a4d8a54286249ec8a75657ee55
Peter Jackson <yooniks@gmail.com>
2018-01-20 16:37:03 -0500

    Websockets work in progress

--- mesh_events/lib/calendar_store.py
+++ mesh_events/lib/calendar_store.py
@@ -1,17 +1,31 @@
 import json
+from random import *
 from .calendar import MeshCalendar, MeshCalendarEncoder
 
-_store = {}
+class CalendarStore:
+  FIRST_NAMES = ['Sophia', 'Jackson', 'Emma', 'Aiden', 'Olivia', 'Lucas', 'Ava', 'Liam', 'Mia', 'Noah', 'Isabella', 'Ethan', 'Riley', 'Mason', 'Aria', 'Caden', 'Zoe', 'Oliver', 'Charlotte', 'Elijah', 'Lily', 'Grayson', 'Layla', 'Jacob', 'Amelia', 'Michael', 'Emily', 'Benjamin', 'Madelyn', 'Carter', 'Aubrey', 'James', 'Adalyn', 'Jayden', 'Madison', 'Logan', 'Chloe', 'Alexander', 'Harper', 'Caleb', 'Abigail', 'Ryan', 'Aaliyah', 'Luke', 'Avery', 'Daniel', 'Evelyn', 'Jack', 'Kaylee', 'William', 'Ella', 'Owen', 'Ellie', 'Gabriel', 'Scarlett', 'Matthew', 'Arianna', 'Connor', 'Hailey', 'Jayce', 'Nora', 'Isaac', 'Addison', 'Sebastian', 'Brooklyn', 'Henry', 'Hannah', 'Muhammad', 'Mila', 'Cameron', 'Leah', 'Wyatt', 'Elizabeth', 'Dylan', 'Sarah', 'Nathan', 'Eliana', 'Nicholas', 'Mackenzie', 'Julian', 'Peyton', 'Eli', 'Maria', 'Levi', 'Grace', 'Isaiah', 'Adeline', 'Landon', 'Elena', 'David', 'Anna', 'Christian', 'Victoria', 'Andrew', 'Camilla', 'Brayden', 'Lillian', 'John', 'Natalie', 'Lincoln']
+  _store = {}
+  _change_number = 0
 
-def add(calendar):
-  _store[calendar.id()] = calendar
+  def add(self, calendar):
+    self._store[calendar.id()] = calendar
+    self._change_number += 1
 
-def all():
-  return list(_store.values())
+  def all(self):
+    return list(self._store.values())
 
-def all_json():
-  return json.dumps(all(), cls=MeshCalendarEncoder)
+  def all_json(self):
+    return json.dumps(self.all(), cls=MeshCalendarEncoder)
 
-def load_seed_data():
-  add(MeshCalendar("James' Work Calendar"))
-  add(MeshCalendar("Neighborhood Events"))
+  def change_number(self):
+    return self._change_number
+
+  def generate_calendar(self):
+    first_name = self.FIRST_NAMES[round(random()*len(self.FIRST_NAMES))]
+    calendar = MeshCalendar(first_name + "'s Calendar")
+    self.add(calendar)
+
+
+  def load_seed_data(self):
+    self.generate_calendar()
+    self.generate_calendar()
--- mesh_events/mesh_events.py
+++ mesh_events/mesh_events.py
@@ -1,20 +1,54 @@
 import atexit
 from datetime import datetime
 
-from flask import Flask, abort, flash, redirect, render_template, request, url_for
+from flask import Flask, abort, jsonify, flash, redirect, render_template, request, url_for
+from flask_sockets import Sockets
 
 from lib.advertiser import MDNSAdvertiser
-import lib.calendar_store as CalendarStore
+from lib.calendar_store import CalendarStore
+
+import time
 
 app = Flask(__name__)
+sockets = Sockets(app)
+
 app.config['DEBUG'] = True
 
-CalendarStore.load_seed_data()
+calendar_store = CalendarStore()
+
+calendar_store.load_seed_data()
 advertiser = MDNSAdvertiser()
 
 @app.route('/')
 def index():
-  return render_template('index.html', calendars=CalendarStore.all(), calendars_json=CalendarStore.all_json())
+  return render_template('index.html', calendars=calendar_store.all(), calendars_json=calendar_store.all_json())
+
+@app.route('/calendars.json')
+def calendars_json():
+  return calendar_store.all_json()
+
+@sockets.route('/calendars.socket')
+def calendars_socket(ws):
+  change_number = -1
+  while not ws.closed:
+    # current_change_number = calendar_store.change_number()
+    # if change_number < current_change_number:
+    #   ws.send(calendar_store.all_json())
+    #   change_number = current_change_number
+    # time.sleep(5)
+
+    current_change_number = calendar_store.change_number()
+    if change_number < current_change_number:
+      ws.send(calendar_store.all_json())
+      change_number = current_change_number
+
+    # message = ws.receive()
+    # ws.send(message)
+
+@app.route('/generate-calendar')
+def generate_calendar():
+  return calendar_store.generate_calendar()
+
 
 
 @atexit.register
@@ -24,4 +58,8 @@ def stop_advertiser():
 
 if __name__ == '__main__':
   advertiser.setup()
-  app.run()
+  # app.run()
+  from gevent import pywsgi
+  from geventwebsocket.handler import WebSocketHandler
+  server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+  server.serve_forever()
